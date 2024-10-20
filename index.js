ed2eded18ab550166a2368eee99aeeee1f2fed57
Sandeep Rana <145690424+sandeeprana0512@users.noreply.github.com>
2024-10-20 18:34:13 +0530

    server

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+let cors = require('cors');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware')
+const PORT = 5000 || process.env.PORT;
+const  hostname = "0.0.0.0";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    
-  </React.StrictMode>
-);
+// User Route
+const userRoute = require('./routes/userRoute');
+const updateRoute = require('./routes/updateRoute')
+const userData = require('./routes/userData')
+
+app.use(cors())
+app.use(express.json())
+
+//const uri = process.env.ATLAS_URI;
+
+mongoose.connect('mongodb+srv://AFZL210:vkMdjrTWgHusBEtx@cluster0.jsq8cav.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log("connected to server")
+}).catch((e) => console.log(e))
 
 
-reportWebVitals();
+// Routes
+app.get('/api/check',(req,res) => {
+    res.json('API is running fine')
+})
+
+
+app.use("/api/user",userRoute)
+
+app.use('/api/update',updateRoute)
+
+app.use('/api/userdata',userData)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT,hostname,(req,res) => {
+    console.log(`server listening at port : ${PORT}`);
+})
